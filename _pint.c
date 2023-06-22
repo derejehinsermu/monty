dd1c9c507031b003e3702a3307aef65ed2d73eb8
derejehinsermu <derejehinsermu2@gmail.com>
2023-06-22 13:01:21 -0700

    fuction:print_stacktop added

---
+++ _pint.c
@@ -0,0 +1,18 @@
+#include "monty.h"
+
+/**
+ * print_stacktop - Prints the value at the top of the stack
+ * @stack: Double pointer to the stack
+ * @line_number: Line number in the file
+ *
+ * Return: void
+ */
+void print_stacktop(stack_t **stack, unsigned int line_number)
+{
+	if (*stack == NULL)
+	{
+		_perror(6, line_number);
+	}
+	printf("%d\n", (*stack)->n);
+}
+
